3340dae11447a3ea314e40e39a55eb48bde08ffb
Juan Giraldo <JUSEGIDU@HOTMAIL.COM>
2022-10-29 16:58:03 -0500

    se hace esto en el archivo gas_stations_controller, en el index. Falta terminarlo

--- app/controllers/gas_stations_controller.rb
+++ app/controllers/gas_stations_controller.rb
@@ -1,8 +1,18 @@
+require 'rest-client'
+require 'json'
 class GasStationsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show, :index]
   def index
-    @location = params[:query]
-    @test = Geocoder.search(@location)
+    # @location = params[:query]
+    # @test = Geocoder.search(@location)
+    # source_coordenates_as_string = @test.first.data["lat"].to_s + "," + @test.first.data["lon"].to_s + ";"
+    # destinations_coordenates_as_string = generate_gas_station_coordenates_string
+    # coordenates = source_coordenates_as_string + destinations_coordenates_as_string
+    #  # link = "https://api.mapbox.com/directions-matrix/v1/mapbox/driving/#{coordenates}?sources=0&destinations=1;2;3;4;5&access_token=pk.eyJ1IjoianVzZWdpZHUiLCJhIjoiY2w2eTZ1M2RoMnl4dTNkcjM4OTY1MHZqbSJ9.-ALdGK2TsDDx3E7zv2ppYw"
+    # link = "https://api.mapbox.com/directions-matrix/v1/mapbox/driving/-122.42,37.78;-122.45,37.91;-122.48,37.73?approaches=curb;curb;curb&access_token=pk.eyJ1IjoianVzZWdpZHUiLCJhIjoiY2w2eTZ1M2RoMnl4dTNkcjM4OTY1MHZqbSJ9.-ALdGK2TsDDx3E7zv2ppYw"
+    # response = RestClient.get(link)
+    # repos = JSON.parse(response)
+    # raise
     if @location
       @stations = GasStation.all
       #  @stations.each do |station|
@@ -73,5 +83,14 @@ class GasStationsController < ApplicationController
   # Only allow a list of trusted parameters through.
   def station_params
     params.require(:gas_station).permit(:name, :address, :latitude, :longitude, :photo)
+  end
+
+  def generate_gas_station_coordenates_string
+    string = ""
+    GasStation.all.pluck(:latitude,:longitude).each do |array|
+      coordenates = array.join(",") + ";"
+      string += coordenates
+    end
+    return string.chop
   end
 end
